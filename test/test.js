8a402d090d730ba4d55b0644eb6bed02bce1fa58
XhmikosR <xhmikosr@gmail.com>
2026-04-13 07:48:59 +0300

    Replace mocha with uvu

--- test/test.js
+++ test/test.js
@@ -1,8 +1,7 @@
-/* eslint-env mocha */
-
 'use strict';
 
 const assert = require('assert').strict;
+const { suite } = require('uvu');
 const detective = require('../index.js');
 
 function hasDependencies(source, expected) {
@@ -10,36 +9,38 @@ function hasDependencies(source, expected) {
   assert.deepEqual(dependencies, expected);
 }
 
-describe('detective-stylus', () => {
-  it('does not throw for empty files', () => {
-    assert.doesNotThrow(() => {
-      detective('');
-    });
-  });
+const test = suite('detective-stylus');
 
-  it('throws if the given content is not a string', () => {
-    assert.throws(() => {
-      detective(() => {});
-    }, /^Error: content is not a string$/);
-  });
-
-  it('throws if called with no arguments', () => {
-    assert.throws(() => {
-      detective();
-    }, /^Error: content not given$/);
-  });
-
-  it('returns the dependencies of Stylus @import statements', () => {
-    hasDependencies('@import "_foo.styl"', ['_foo.styl']);
-    hasDependencies('@import "_foo"', ['_foo']);
-    hasDependencies('body { color: blue } @import "_foo"', ['_foo']);
-    hasDependencies('@import "bar"', ['bar']);
-    hasDependencies('@import "_foo.styl";\n@import "_bar.styl"', ['_foo.styl', '_bar.styl']);
-    hasDependencies('@import "_foo.styl"\n@import "_bar.styl"\n@import "_baz"\n@import "_buttons"', ['_foo.styl', '_bar.styl', '_baz', '_buttons']);
-  });
-
-  it('returns the dependencies of Stylus @require statements', () => {
-    hasDependencies('@require \'bar\';', ['bar']);
-    hasDependencies('@require \'bar.styl\';', ['bar.styl']);
+test('does not throw for empty files', () => {
+  assert.doesNotThrow(() => {
+    detective('');
   });
 });
+
+test('throws if the given content is not a string', () => {
+  assert.throws(() => {
+    detective(() => {});
+  }, /^Error: content is not a string$/);
+});
+
+test('throws if called with no arguments', () => {
+  assert.throws(() => {
+    detective();
+  }, /^Error: content not given$/);
+});
+
+test('returns the dependencies of Stylus @import statements', () => {
+  hasDependencies('@import "_foo.styl"', ['_foo.styl']);
+  hasDependencies('@import "_foo"', ['_foo']);
+  hasDependencies('body { color: blue } @import "_foo"', ['_foo']);
+  hasDependencies('@import "bar"', ['bar']);
+  hasDependencies('@import "_foo.styl";\n@import "_bar.styl"', ['_foo.styl', '_bar.styl']);
+  hasDependencies('@import "_foo.styl"\n@import "_bar.styl"\n@import "_baz"\n@import "_buttons"', ['_foo.styl', '_bar.styl', '_baz', '_buttons']);
+});
+
+test('returns the dependencies of Stylus @require statements', () => {
+  hasDependencies('@require \'bar\';', ['bar']);
+  hasDependencies('@require \'bar.styl\';', ['bar.styl']);
+});
+
+test.run();
